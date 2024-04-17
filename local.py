9a9baba5af4bd6be00f1f46a401d2b7fd2a8f3ad
Dai TENSAOUT <123576246+DaiTensa@users.noreply.github.com>
2024-04-17 14:07:13 +0200

    Update local.py from remote

--- local.py
+++ local.py
@@ -2,3 +2,5 @@ print("Hello Simplon")
 print("modification qui vient de remote")
 print("nouveau changement en local")
 print("nouveau changement depuis remote")
+
+print("modif sur local avec pull request en attente")
